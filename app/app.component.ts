a638a1b98abadf585e35da6efce355605a2af207
Marco Bassaletti <bassaletti@gmail.com>
2016-10-19 02:36:09 -0300

    Initial commit of Angular2 quickstart tutorial. No drama for now.

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by mbassale on 19-10-16.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular2 App</h1><small>(I\'ve developed other apps in angular 1.x)</small>'
+})
+export class AppComponent { }
